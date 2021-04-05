01b0d4290b166e3c342518419f8511e33a447b5d
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-05 03:22:50 -0300

    finalizando tela activity login

--- SorvetunesApp/app/src/main/java/br/com/sorvetunes/sorvetunesapp/MainActivity.kt
+++ SorvetunesApp/app/src/main/java/br/com/sorvetunes/sorvetunesapp/MainActivity.kt
@@ -2,11 +2,14 @@ package br.com.sorvetunes.sorvetunesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-
+        imageview_login.setImageResource(R.drawable.img_sorvete_01)
     }
+
 }
