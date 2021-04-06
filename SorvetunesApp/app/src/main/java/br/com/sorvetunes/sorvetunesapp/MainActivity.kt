de3575e917d707b612af7f2e9277b162b452930e
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-06 00:44:31 -0300

    criando validações.

--- SorvetunesApp/app/src/main/java/br/com/sorvetunes/sorvetunesapp/MainActivity.kt
+++ SorvetunesApp/app/src/main/java/br/com/sorvetunes/sorvetunesapp/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.sorvetunes.sorvetunesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.login.*
+import android.widget.Toast
 
 class MainActivity : DebugActivity() {
 
@@ -10,6 +11,15 @@ class MainActivity : DebugActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
         imageview_login.setImageResource(R.drawable.img_sorvete_01)
+
+        botao_login.setOnClickListener{
+            val user  = input_user.text.toString()
+            val password = input_password.text.toString()
+            if (user.equals("") && password.equals("")){
+                Toast.makeText(this, "Usuário e Senha vazios, digite um valor válido para realizar Login !", Toast.LENGTH_LONG).show()
+            } else{Toast.makeText(this, "Clicou no Login", Toast.LENGTH_LONG).show() }
+
+        }
     }
 
 }
