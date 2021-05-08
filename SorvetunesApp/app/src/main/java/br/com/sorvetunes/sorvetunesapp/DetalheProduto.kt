c17fc5b1ad752ac8a993c68381826e0652c7ce95
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-05-08 14:49:44 -0300

    detalhe parte 2

--- SorvetunesApp/app/src/main/java/br/com/sorvetunes/sorvetunesapp/DetalheProduto.kt
+++ SorvetunesApp/app/src/main/java/br/com/sorvetunes/sorvetunesapp/DetalheProduto.kt
@@ -2,12 +2,22 @@ package br.com.sorvetunes.sorvetunesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detalhe_produto.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class DetalheProduto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhe_produto)
+
+        var produto = intent.getSerializableExtra("produto") as Produtos
+
+        campo_nome.text = produto.nome
+        campo_descricao.text = produto.descricao
+        campo_valor_produto.text = produto.valor
+
+
+
 
     }
 
